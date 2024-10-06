bfbdfefd7a65b1cfce6d7eeb73e3ef5edab8ce42
c3c1l1a <cesswangumukima@gmail.com>
2024-10-06 14:57:44 +0800

    Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 const menu = document.getElementById("menu")
+const  closeIcon = document.getElementById("close-menu")
+const nav = document.getElementById("nav")
+const navLinks = document.getElementById("nav-links")
 menu.addEventListener("click", function(){
-    const nav = document.getElementById("nav")
     nav.style.cssText = `
         flex-direction: column;
         height: 100vh;
     `
-    const navLinks = document.getElementById("nav-links")
     navLinks.style.cssText = `
         display:flex;
         flex-direction: column;
-    `
+        z-index: -1;
+        ;`
+    
+    menu.style.display = "none"
+    closeIcon.style.display = "block";
 })
+
+closeIcon.addEventListener("click", function(){
+    menu.style.display = "block";
+    closeIcon.style.display = "none";
+    navLinks.style.display = "none";
+})
+
+const navItems = document.getElementsByClassName("nav-item");
+let animationDelay = 0;
+
+for (let i = 0; i < navItems.length; i++){
+    console.log(animationDelay + "s");
+    console.log(navItems[i])
+    navItems[i].style.animationDelay = animationDelay + "s";
+    animationDelay += 0.1;
+}
